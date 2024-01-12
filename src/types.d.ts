2228c704e4505eb2fa9139ce0e108347eba5e605
Valentin Kiselev <mrexox@evilmartians.com>
2024-01-12 12:43:50 +0300

    fix: types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from 'express';
+
 export interface HttpFunction {
   // eslint-disable-next-line no-unused-vars
   (request: Request, response: Response): Promise<void>;
